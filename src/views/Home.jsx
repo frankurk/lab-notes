6f82ddd8d9c20d4df18d60f1baba200ea5700c85
soniarez <101676781+soniarez@users.noreply.github.com>
2022-09-05 21:32:38 -0300

    grid in home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,18 +6,20 @@ import NotesContainer from '../components/NotesContainer';
 const Home = () => {
   return (
     <div
-      className=" grid grid-cols-3 auto-rows-max
-    border-4 border-rose-500 w-full h-full"
+      className=" 
+    border-4 border-rose-500 w-full h-screen"
     >
-      <div className="col-span-3 border-4 border-orange-500 ">
+      <div className="border-4 border-orange-500 h-[8%]">
         <Header />
       </div>
-      <div className="col-span-1 row-span-5 border-4 border-blue-500 ">
-        <Sidebar />
-      </div>
 
-      <div className="col-span-2 row-span-5 border-4 border-yellow-500  ">
-        <NotesContainer />
+      <div className=" flex flex-row border-4 border-yellow-500 h-[92%] ">
+        <div className=" basis-20 border-4 border-blue-500 bg-white dark:bg-gray-800 ">
+          <Sidebar />
+        </div>
+        <div className="basis-full border-4 border-green-500">
+          <NotesContainer />
+        </div>
       </div>
     </div>
   );
