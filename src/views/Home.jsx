48c44e580ec789ae1874fe0f55deeb0ee0cb0567
Francisca Caro <francisca@caro.cl>
2022-09-06 12:16:34 -0400

    flex in home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,21 +5,15 @@ import NotesContainer from '../components/NotesContainer';
 
 const Home = () => {
   return (
-    <div
-      className=" 
-    border-4 border-rose-500 w-full h-screen"
-    >
-      <div className="border-4 border-orange-500 h-[8%]">
+    <div className="flex flex-row flex-wrap border-4 border-rose-500 w-full h-screen">
+      <div className="border-4 border-orange-500 h-[8%] w-full">
         <Header />
       </div>
-
-      <div className=" flex flex-row border-4 border-yellow-500 h-[92%] ">
-        <div className=" basis-20 border-4 border-blue-500 bg-white dark:bg-gray-800 ">
-          <Sidebar />
-        </div>
-        <div className="basis-full border-4 border-green-500">
-          <NotesContainer />
-        </div>
+      <div className="border-4 border-blue-500 bg-white dark:bg-gray-800 w-[20%] h-[92%]">
+        <Sidebar />
+      </div>
+      <div className="border-4 border-green-500 w-[80%] h-[92%]" >
+        <NotesContainer />
       </div>
     </div>
   );
