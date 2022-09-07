2d1e6180094b0973728d2c7d1c91bcc8749c7bd9
Francisca Caro <francisca@caro.cl>
2022-09-07 12:20:26 -0400

    static version header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/images/Dark.png';
+import Avatar from '../assets/images/Avatar.png';
 
 const Header = () => {
+
+  const [menu, setMenu] = useState(false);
+
+
   return (
-    <div className='flex flex-row justify-between p-4 bg-white dark:bg-gray-800 text-white '>
-      <div className='flex flex-row'>
-        <div>Logo</div>
-        <h3>LabNotes</h3>
+    <div className="flex flex-row justify-between items-center text-white py-1 px-4">
+      <div className="flex flex-row items-center">
+        <img src={Logo} className="w-12 h-12 mx-2" />
+        <h3 className="text-xl font-semibold">LabNotes</h3>
       </div>
-      <div className='flex flex-row'>
-        <div>Avatar</div>
-        <div> Hamburger Menu</div>
+      <div>
+        <button
+        onClick={() => setMenu(!menu)}
+          type="button"
+        >
+           <img src={Avatar} className="w-12 h-12 mx-2" />
+        </button>
+       {menu ? <div
+          className="fixed right-4 z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
+        >
+          <ul
+            className="py-1 text-sm text-gray-700 dark:text-gray-200"
+          >
+            <li>
+              <Link
+                to="#"
+                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Settings
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="#"
+                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Sign out
+              </Link>
+            </li>
+          </ul>
+        </div> : null}
+        
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,31 +11,31 @@ const Sidebar = () => {
           <div >
             <nav className="mt-10 px-6 ">
               <Link to="/"
-                className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg "
+                className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg"
                 
               >
-                <span className="mx-4 text-lg font-normal">Element</span>
+                <span className="mx-4 text-lg">Element</span>
                 <span className="flex-grow text-right"></span>
               </Link>
               <Link to="#"
                 className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-800 dark:text-gray-100 rounded-lg bg-gray-100 dark:bg-gray-600"
                 
               >
-                <span className="mx-4 text-lg font-normal">Form</span>
+                <span className="mx-4 text-lg">Form</span>
                 <span className="flex-grow text-right"></span>
               </Link>
               <Link to="#"
                 className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg "
                
               >
-                <span className="mx-4 text-lg font-normal">Commerce</span>
+                <span className="mx-4 text-lg">Commerce</span>
                 <span className="flex-grow text-right"></span>
               </Link>
               <Link to="#"
                 className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg "
                 
               >
-                <span className="mx-4 text-lg font-normal">Navigation</span>
+                <span className="mx-4 text-lg">Navigation</span>
                 <span className="flex-grow text-right"></span>
               </Link>
             </nav>
