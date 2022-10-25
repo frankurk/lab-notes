813a542d6680c1c87354caa1e7300b15316146a6
frankurk <francisca@caro.cl>
2022-10-25 14:47:16 -0300

    first tests

---
+++ src/tests/Header.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    describe, expect, it, vi,
+} from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Header from '../components/Header.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+/**
+* @vitest-environment jsdom
+*/
+
+it('renders labnotes title', () => {
+    render(<Router><Header /></Router>);
+    const title = screen.getByText('LabNotes');
+    expect(title).toBeInTheDocument();
+})
+
+it('renders avatar button', () => {
+    render(<Router><Header /></Router>);
+    const avatarButton = screen.getByRole('button', { src: 'Avatar' });
+    expect(avatarButton).toBeInTheDocument();
+})
+
+it('shows menu when clicked', () => {
+    render(<Router><Header /></Router>);
+    const avatarButton = screen.getByRole('button');
+    const settingsOption = screen.getByRole('button');
+
+    fireEvent.click(avatarButton);
+    expect(settingsOption).toBeInTheDocument();
+})
